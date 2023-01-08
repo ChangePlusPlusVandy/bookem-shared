a75dcd54294d739754bd658f8b8c940e162f2a6c
Zi Nean Teoh <zinean00@gmail.com>
2023-01-08 09:51:25 -0600

    specify re-export models

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export * as database from './types/database';
-export * as models from './models';
+export * as Users from './models/Users';
+export * as VolunteerLogs from './models/VolunteerLogs';
+export * as VOlunteerProgramApplications from './models/VolunteerProgramApplications';
+export * as VolunteerPrograms from './models/VolunteerPrograms';
