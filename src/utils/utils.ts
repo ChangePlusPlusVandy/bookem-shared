83ada6cafc0979c30a92e53ba85daefc3117fab3
Zi Nean Teoh <zinean00@gmail.com>
2023-04-01 22:06:58 -0500

    add utils

---
+++ src/utils/utils.ts
@@ -0,0 +1,10 @@
+import { VolunteerProgramLocation } from '../types/database';
+
+/**
+ * Helper function to convert location to string
+ * @param location The location of the program
+ * @returns The location in string format
+ */
+const convertLocationToString = (location: VolunteerProgramLocation) => {
+  return `${location.street} ${location.city}, ${location.state} ${location.zip}`;
+};
