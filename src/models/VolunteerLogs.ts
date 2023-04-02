07820fc463fc55dc47ec87388e9f3e15201c8876
Zi Nean Teoh <zinean00@gmail.com>
2023-04-02 17:04:25 -0500

    update volunteer logs

--- src/models/VolunteerLogs.ts
+++ src/models/VolunteerLogs.ts
@@ -4,17 +4,33 @@ import { VolunteerLogData } from '../types/database';
 // VolunteerLogSchema describes what our documents should look like in our VolunteerLogs collections
 const VolunteerLogSchema = new mongoose.Schema<VolunteerLogData>(
   {
-    school: { type: String, required: false },
-    teacher: { type: String, required: false },
-    date: { type: Date, required: true },
-    hours: { type: Number, required: true },
+    /**
+     * The user who logged this event
+     */
     userId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
       required: true,
     },
-    feedback: { type: String, required: false },
+
+    /**
+     * The event that this log is for
+     */
+    eventId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'VolunteerEvent',
+      required: true,
+    },
+
+    /**
+     * Basic log information
+     */
+    date: { type: Date, required: true },
+    hours: { type: Number, required: true },
     numBooks: { type: Number, default: 0, required: true },
+    school: { type: String },
+    teacher: { type: String },
+    feedback: { type: String },
   },
   {
     timestamps: {
