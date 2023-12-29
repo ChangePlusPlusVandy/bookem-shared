1836b50b38ca121751f9e995d3f5a377f75aa5c5
David Huang <davidhuang1203@hotmail.com>
2023-12-28 22:36:51 -0600

    separate fake data scripts from bookem-users
    in our dependency, faker and bcrypt are for generating fake data
    dotenv is for getting MongoDB connection str from local env

---
+++ src/scripts/01-insert-accounts.ts
@@ -0,0 +1,31 @@
+import dbConnect from './dbConnectForScript';
+import Admins from '../models/Admins';
+import Users from '../models/Users';
+import { generateAdmin, generateUser } from './helper-functions';
+
+async function main() {
+  try {
+    // Connect to the database
+    await dbConnect();
+
+    // Delete all users and admins
+    await Users.deleteMany({});
+    await Admins.deleteMany({});
+
+    // Insert user
+    const user = await generateUser({});
+    await Users.insertMany(user);
+
+    // Insert admin
+    const admin = await generateAdmin();
+    await Admins.insertMany(admin);
+
+    console.log(
+      'Successfully inserted a user and an admin to db. You can terminate this script now.'
+    );
+  } catch (error: any) {
+    console.error('Error:', error.message);
+  }
+}
+
+main();
---
+++ src/scripts/02-insert-tags-programs.ts
@@ -0,0 +1,37 @@
+import dbConnect from './dbConnectForScript';
+import Tags from '../models/Tags';
+import VolunteerPrograms from '../models/VolunteerPrograms';
+import { INSERTED_PROGRAMS, INSERTED_TAGS } from './constants';
+import { generateProgram, generateTag } from './helper-functions';
+
+async function main() {
+  try {
+    // Connect to the database
+    await dbConnect();
+
+    // TODO:
+    // delete all tags
+    await Tags.deleteMany({});
+
+    // delete all programs
+    await VolunteerPrograms.deleteMany({});
+
+    // Generate programs
+    INSERTED_PROGRAMS.forEach(async program => {
+      const generatedProgram = generateProgram(program);
+      await VolunteerPrograms.insertMany(generatedProgram);
+    });
+
+    // Generate tags
+    INSERTED_TAGS.forEach(async tag => {
+      const genratedTag = generateTag(tag);
+      await Tags.insertMany(genratedTag);
+    });
+
+    console.log('Success! You can terminate this script now.');
+  } catch (error: any) {
+    console.error('Error:', error.message);
+  }
+}
+
+main();
