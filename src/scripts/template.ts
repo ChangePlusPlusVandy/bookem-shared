acad3d70d926e7a83250ed9442fca3f57277c183
Harry Huang <jiashu.huang@vanderbilt.edu>
2024-01-16 14:53:35 -0600

    Add template.ts

--- src/scripts/template.ts
+++ src/scripts/template.ts
@@ -1,29 +1,11 @@
-import dbConnect from '@/lib/dbConnect';
-import { NextApiRequest, NextApiResponse } from 'next';
+import scriptDbConnect from './dbConnectForScript';
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  switch (req.method) {
-    case 'GET':
-      try {
-        // Connect to the database
-        await dbConnect();
+async function main() {
+  await scriptDbConnect();
 
-        // TODO:
+  // Data operations here
 
-        res
-          .status(200)
-          .json({ success: true, message: 'This is a template endpoint' });
-      } catch (error: any) {
-        res.status(500).json({ success: false, error: error.message });
-      }
-
-      break;
-
-    default:
-      res.status(400).json({ success: false });
-      break;
-  }
+  console.log('Success! You can terminate this script now.');
 }
+
+main();
